e8749416f97f3de15bf12f6f48879336e07b6dd2
paulooorg <goncalvesrpaulo@gmail.com>
2020-04-23 20:21:01 -0300

    Add I18n Message Reader

--- api/src/main/java/io/github/paulooorg/api/infrastructure/i18n/Messages.java
+++ api/src/main/java/io/github/paulooorg/api/infrastructure/i18n/Messages.java
@@ -1,7 +1,32 @@
 package io.github.paulooorg.api.infrastructure.i18n;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.text.MessageFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Messages {
-    public String get(String key) {
-        return key;
+    private static final ResourceBundle messagesFile = ResourceBundle.getBundle("i18n/messages", Locale.getDefault());
+
+    private static final Logger logger = LogManager.getLogger(Messages.class);
+
+    public static String get(String key) {
+        try {
+            return messagesFile.getString(key);
+        } catch (Exception e) {
+            logger.error("Value not found for key {}", key);
+            return key;
+        }
+    }
+
+    public static String get(String key, Object... parameters) {
+        try {
+            return MessageFormat.format(messagesFile.getString(key), parameters);
+        } catch (Exception e) {
+            logger.error("Value not found for key {}", key);
+            return key;
+        }
     }
 }
